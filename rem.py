436228147d733429107e861bc12e426e837e5832
Velakenna <141400352+Velakenna@users.noreply.github.com>
2023-08-29 11:46:41 +0530

    Update rem.py

--- rem.py
+++ rem.py
@@ -21,10 +21,6 @@ def calculate_reminder_dates(purchase_date):
     renewal_date = purchase_date + datetime.timedelta(days=32)
     return reminder_date, renewal_date
 
-def send_renewal_request_to_channel(user_id, username):
-    # Send a message to the channel requesting renewal for the user
-    app.send_message(bot_owner_chat_id, f"Renewal request for user {username} (ID: {user_id}): Renewal is due today!")
-
 @app.on_message(filters.group & ~filters.edited)
 def group_message_handler(client, message):
     try:
@@ -42,33 +38,6 @@ def group_message_handler(client, message):
     except Exception as e:
         print("An error occurred in group_message_handler:", e)
     
-def check_reminders():
-    while True:
-        try:
-            current_time = datetime.datetime.now()
-            for user_id, purchase_info in vps_purchase_details.items():
-                purchase_date = purchase_info["purchase_date"]
-                reminder_date, renewal_date = calculate_reminder_dates(purchase_date)
-                if current_time.date() == reminder_date.date():
-                    keyboard = InlineKeyboardMarkup([
-                        [InlineKeyboardButton("Renew Now", callback_data="renew_now")]
-                    ])
-                    app.send_message(user_id, f"Reminder: Your VPS renewal is due tomorrow. Would you like to renew now?", reply_markup=keyboard)
-                elif current_time.date() == renewal_date.date():
-                    send_renewal_request_to_channel(user_id, purchase_info["username"])
-            time.sleep(3600)  # Check every hour
-        except Exception as e:
-            print("An error occurred in check_reminders:", e)
-          
-@app.on_callback_query()
-def callback_handler(client, callback_query):
-    try:
-        if callback_query.data == "renew_now":
-            user_id = callback_query.from_user.id
-            app.send_message(user_id, "Please proceed with the renewal process.")
-            # You can add further logic here for handling the renewal process
-    except Exception as e:
-        print("An error occurred in callback_handler:", e)
 
 if __name__ == "__main__":
     try:
